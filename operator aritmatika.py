a9bc05227c9adb4bb505d4dc82e4965bffa78352
tanpabatasbro <115479649+tanpabatasbro@users.noreply.github.com>
2022-10-19 19:34:10 +0800

    Add files via upload

---
+++ operator aritmatika.py
@@ -0,0 +1,25 @@
+#operator aritmatika
+
+angka_1 = int(input("masukkan angka pertama :"))
+angka_2 = int(input("masukkan angka kedua :"))
+#penjumlahan
+hasil = angka_1 + angka_2
+print("{} + {} = {}". format(angka_1,angka_2,hasil))
+#pengurangan
+hasil = angka_1 - angka_2
+print("{} - {} = {}". format(angka_1,angka_2,hasil))
+#perkalian
+hasil = angka_1 * angka_2
+print("{} * {} = {}". format(angka_1,angka_2,hasil))
+#pembagian
+hasil = angka_1 / angka_2
+print("{} / {} = {}". format(angka_1,angka_2,hasil))
+#sisa bagi
+hasil = angka_1 % angka_2
+print("{} % {} = {}". format(angka_1,angka_2,hasil))
+#pangkat
+hasil = angka_1 ** angka_2
+print("{} ** {} = {}". format(angka_1,angka_2,hasil))
+#pembagian bulat
+hasil = angka_1 // angka_2
+print("{} // {} = {}". format(angka_1,angka_2,hasil))
